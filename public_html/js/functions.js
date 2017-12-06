0988c1121ea6e5dd4a6e3baa727e03dad5170d41
Ilich Arredondo <ilichh1@hotmail.com>
2017-12-05 18:35:38 -0600

    Añadido Mapbox

--- public_html/js/functions.js
+++ public_html/js/functions.js
@@ -3,13 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$("#enviarZip").click(function(){
-    
-    
-    
+const GLOBAL = {
+    zipAPIURL: "https://api-codigos-postales.herokuapp.com/v2/codigo_postal/"
+};
+
+$("#enviarZip").click(function () {
+    let cpBuscar = $("#zipCode").val();
+
+    $.get(
+            GLOBAL.zipAPIURL + cpBuscar,
+            {},
+            function (response) {
+                //window.console.log(response);
+                let query = response.municipio + ", " + response.estado;
+                window.console.log(query);
+            });
 });
 
-var callbackFunction = function(data) {
-    var wind = data.query.results.channel.wind;
-    alert(wind.chill);
-};
+function init() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia2FybG96aHMiLCJhIjoiY2phdTZyYTF3MDBqcTMzbmduam81dGcyZiJ9.zfzpreVgLruGLidnllLhkg';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9'
+    });
+}
+
+$(init());
